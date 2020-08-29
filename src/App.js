9894aa2c2829292979d8071821b050c226159129
Alexander <alexander.ayras@gmail.com>
2020-08-29 11:57:39 -0500

    Agregando Emotion

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Contenedor = styled.div`
+	display: flex;
+	align-items: center;
+	padding-top: 5rem;
+	flex-direction: column-reverse;
+`
+
+const Boton = styled.button`
+	background: -webkit-linear-gradient(
+		top left,
+		#007d35 0%,
+		#007d35 40%,
+		#0f574e 100%
+	);
+	background-size: 300px;
+	font-family: Arial, Helvetica, sans-serif;
+	color: #fff;
+	margin-top: 3rem;
+	padding: 1rem 3rem;
+	font-size: 2rem;
+	border: 2px solid black;
+`
+
+const consultandoAPI = () => {
+	console.log('Consultando...')
 }
 
-export default App;
+function App() {
+	return (
+		<Contenedor>
+			<Boton onClick={consultandoAPI}>Obtener Frase</Boton>
+		</Contenedor>
+	)
+}
+
+export default App
